63c79dd8392dd40dcc6c67d20b3c5a07d0e7eb4e
Will-Wu1993 <33507605+Will-Wu1993@users.noreply.github.com>
2020-04-01 22:00:56 +0800

    Initial commit

---
+++ src/main/java/com/willwu/fastgithub/FastgithubApplication.java
@@ -0,0 +1,13 @@
+package com.willwu.fastgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastgithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastgithubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/willwu/fastgithub/FastgithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willwu.fastgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastgithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
